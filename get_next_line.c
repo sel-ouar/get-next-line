60f764dad55d8a135cc2637c9f69a9f680134dba
sel-ouar <116719407+sel-ouar@users.noreply.github.com>
2022-12-20 14:22:31 +0100

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-ouar <sel-ouar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/18 15:45:27 by sel-ouar          #+#    #+#             */
+/*   Updated: 2022/12/20 13:27:43 by sel-ouar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_free(char *save)
+{
+	free(save);
+	return (NULL);
+}
+
+char	*ft_line(char **save)
+{
+	char	*line;
+
+	if (!save[0][0])
+	{
+		*save = ft_free(*save);
+		return (NULL);
+	}
+	line = ft_strdup(*save);
+	if (!line)
+		return (NULL);
+	*save = ft_free(*save);
+	return (line);
+}
+
+int	check_error(int fd)
+{
+	if (fd < 0 || fd > OPEN_MAX)
+		return (1);
+	if (BUFFER_SIZE <= 0)
+		return (1);
+	return (0);
+}
+
+char	*readline(int fd, char *save)
+{
+	ssize_t	ret;
+	char	*buffer;
+	char	*temp;
+
+	ret = 0;
+	buffer = malloc ((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	if (!save)
+		save = ft_strdup("");
+	while (ft_strchr(save, '\n') == NULL)
+	{
+		ret = read(fd, buffer, BUFFER_SIZE);
+		if (ret < 0)
+			return (ft_free(save), free(buffer), NULL);
+		if (ret == 0 || buffer[0] == '\0')
+			break ;
+		buffer[ret] = '\0';
+		temp = save;
+		save = ft_strjoin(save, buffer);
+		free (temp);
+	}
+	return (free(buffer), save);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*save;
+	char		*line;
+	char		*temp;
+	size_t		len;
+
+	if (check_error(fd))
+		return (NULL);
+	save = readline(fd, save);
+	if (!save)
+		return (NULL);
+	if (!(ft_strchr(save, '\n')))
+	{
+		line = ft_line(&save);
+		return (line);
+	}
+		len = ft_strlen(save) - ft_strlen(ft_strchr(save, '\n') + 1);
+		temp = save;
+		line = ft_substr(save, 0, len);
+		save = ft_strdup(save + len);
+		temp = ft_free(temp);
+	return (line);
+	return (NULL);
+}
